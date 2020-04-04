a56cd76ee4caac21e06c5d9748f2c2cb2082446f
Konstantin Malyshev <konstantin.malyshev@indriver.com>
2020-04-05 01:36:36 +0900

    init

---
+++ HomeWork1/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HomeWork1
+//
+//  Created by Константин Малышев on 05.04.2020.
+//  Copyright © 2020 Константин Малышев. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork1Tests/HomeWork1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HomeWork1Tests.swift
+//  HomeWork1Tests
+//
+//  Created by Константин Малышев on 05.04.2020.
+//  Copyright © 2020 Константин Малышев. All rights reserved.
+//
+
+import XCTest
+@testable import HomeWork1
+
+class HomeWork1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
